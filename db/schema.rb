7176130dbcf55350225399fbaff8cd239cd84710
Aimeric Marchau <marchauaimeric@gmail.com>
2023-12-17 17:31:41 +0100

    create family and family_member models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_13_113500) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_162742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "family_members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "family_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_family_members_on_family_id"
+    t.index ["user_id"], name: "index_family_members_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_113500) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "family_members", "families"
+  add_foreign_key "family_members", "users"
 end
