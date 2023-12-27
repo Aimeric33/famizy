e59a3c698fb369f9894f589ffcb220cb3e4a2bd5
Aimeric Marchau <marchauaimeric@gmail.com>
2023-12-27 13:09:52 +0100

    locales configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#home"
+  get '/:locale', to: "pages#home"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
 
-  resources :families, except: %i[index] do
-    resources :family_members, only: %i[create destroy]
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
+
+    resources :families, except: %i[index] do
+      resources :family_members, only: %i[create destroy]
+    end
   end
 end
