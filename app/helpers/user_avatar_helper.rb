9a24d14786aac9bab45571dea16572503f2d283c
Aimeric Marchau <marchauaimeric@gmail.com>
2024-04-23 14:32:54 +0200

    user avatar

--- app/helpers/user_avatar_helper.rb
+++ app/helpers/user_avatar_helper.rb
@@ -1,7 +1,7 @@
 module UserAvatarHelper
   def user_avatar(user)
     if user.avatar.attached?
-      cl_image_tag user.avatar.key, class: 'object-cover w-8 h-8 rounded-full'
+      cl_image_tag user.avatar.key, class: 'object-cover rounded-full'
     else
       "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"100%\" height=\"100%\" viewBox=\"0 0 40 40\">
         <rect width=\"100%\" height=\"100%\" fill=\"#{user.color}\"/>
