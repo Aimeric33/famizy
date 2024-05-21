006dee1f7c5aa41aed66bcc797959f2b7d23482d
Aimeric Marchau <marchauaimeric@gmail.com>
2024-05-21 20:42:05 +0200

    display meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,7 +5,8 @@ class Meal < ApplicationRecord
   enum meal_type: { lunch: 0, dinner: 1}
 
   validates :date, presence: true
-  # validates :recipe, presence: true
+  validates :recipe, presence: true, unless: -> { other_name.present? }
+  validates :other_name, presence: true, unless: -> { recipe.present? }
   validates :meal_type, presence: true
 
   def self.meals_this_week(family)
