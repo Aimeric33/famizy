51ff1c6840a737f3b5d1a6daad89909160a1bb47
Aimeric Marchau <marchauaimeric@gmail.com>
2024-06-09 18:42:30 +0200

    filters v2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,9 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = policy_scope(Recipe).where(family_id: Family.find(params[:family_id]))
+    if params[:filter].present?
+      @recipes = @recipes.where(recipe_type: params[:filter])
+    end
   end
 
   def show
