91b17fc3fdd12de590aad42edb9af28bfb163856
Aimeric Marchau <marchauaimeric@gmail.com>
2024-06-10 12:34:37 +0200

    fix recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,9 @@
 class RecipesController < ApplicationController
   layout 'dashboard'
+  before_action :set_family
 
   def index
-    @recipes = policy_scope(Recipe).where(family_id: Family.find(params[:family_id]))
+    @recipes = policy_scope(Recipe).where(family_id: @family)
     if !params[:type].present? || (params[:type].present? && params[:type] == 'all')
       @recipes
     else
@@ -11,20 +12,20 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Family.find(session[:current_family]).recipes.find(params[:id])
+    @recipe = @family.recipes.find(params[:id])
     authorize @recipe
   end
 
   def new
     @recipe = Recipe.new
-    @recipe.family = Family.find(params[:family_id])
+    @recipe.family = @family
     @recipe.ingredients.build
     authorize @recipe
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.family = Family.find(params[:family_id])
+    @recipe.family = @family
     authorize @recipe
 
     if @recipe.save
@@ -36,15 +37,14 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Family.find(session[:current_family]).recipes.find(params[:id])
+    @recipe = @family.recipes.find(params[:id])
     @recipe.ingredients.build if @recipe.ingredients.empty?
     authorize @recipe
   end
 
   def update
-    @recipe = Family.find(session[:current_family]).recipes.find(params[:id])
+    @recipe = @family.recipes.find(params[:id])
     authorize @recipe
-
 
     if @recipe.update(recipe_params)
       redirect_to @recipe, notice: t('recipe.updated')
@@ -55,7 +55,7 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    @recipe = Family.find(session[:current_family]).recipes.find(params[:id])
+    @recipe = @family.recipes.find(params[:id])
     authorize @recipe
 
     @recipe.destroy
@@ -66,5 +66,13 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:name, :description, :photo, :recipe_type, ingredients_attributes: [:id, :name, :quantity, :unit, :_destroy])
+  end
+
+  def set_family
+    if params[:family_id].present?
+      @family = Family.find(params[:family_id])
+    else
+      @family = Family.find(session[:current_family])
+    end
   end
 end
